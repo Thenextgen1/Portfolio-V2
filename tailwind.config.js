278cb3976a5c08b2296029fbf12c83d4bf1ef4f0
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-01 17:42:12 +0100

    added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Oxygen: ["Oxygen"]
+      }
+    },
   },
   plugins: [],
 }
