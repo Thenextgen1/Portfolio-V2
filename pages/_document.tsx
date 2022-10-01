f1bbaf55107c7c095fa6140bb025dfbb2c3f4b70
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-01 21:05:26 +0100

    refactored base code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,19 +5,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="theme-color" content="#000000" />
-  <meta name="Vidom" content="Online Course Marketplace" />
-  <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta name="Elijah Ohiwerei" content="Software Developer" />
+          <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
 
-  <link rel="manifest" href="/manifest.json" />
- 
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+          <link rel="manifest" href="/manifest.json" />
+
+          <body>
+            <Main />
+            <NextScript />
+          </body>
         </Head>
 
       </Html>
