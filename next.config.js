bffbefa546eb38e4d67d81d619442f3b98f38268
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-01 17:41:38 +0100

    added service worker config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
+  // next.js config
+})
