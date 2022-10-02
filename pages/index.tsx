8bbf823733e29c4a4ef96a10b2b136465f085c6d
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-02 22:39:05 +0100

    built main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from "next";
 import BaseLayout from "../src/layout";
+import styles from "../src/styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <BaseLayout>
-      <main>
-        <h1>Welcome to</h1>
+      <main className="ml-[17%]">
+        <p className="text-xs tracking-wide font-light py-2">LAGOS</p>
+        <p className="text-xs tracking-wide font-light">21</p>
+        <div className="mt-12">
+          <h2 className="text-8xl tracking-wider font-extrabold">
+            SOFTWARE <br />{" "}
+            <span className={`${styles.text_stroke}`}> DEVELOPER</span>
+          </h2>
+        </div>
       </main>
     </BaseLayout>
   );
