e413e4766987e43545ef2c665f783fbe30b22c10
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-03 15:43:47 +0100

    refactored base code

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,18 @@ import styles from "../src/styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <BaseLayout>
-      <main className="ml-[17%]">
+      <main className="lg:ml-[17%]">
         <p className="text-xs tracking-wide font-light py-2">LAGOS</p>
-        <p className="text-xs tracking-wide font-light">21</p>
+        <p className="text-xs tracking-wide font-light">21&apos;</p>
         <div className="mt-12">
-          <h2 className="text-8xl tracking-wider font-extrabold">
+          <h2 className="lg:text-[90px] tracking-wide font-bold font-Synocopate leading-none">
             SOFTWARE <br />{" "}
             <span className={`${styles.text_stroke}`}> DEVELOPER</span>
           </h2>
         </div>
+        <p className="text-xs lg:absolute lg:bottom-0 lg:mb-[5%] lg:ml-[1px]">
+          MERN STACK DEVELOPER & DATA ANALYST
+        </p>
       </main>
     </BaseLayout>
   );
