d07864f9c7c23498975a9fde4fcef56fe8657ffc
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-03 15:43:33 +0100

    fixed meta viewport bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,21 +5,55 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#000000" />
-          <meta name="Elijah Ohiwerei" content="Software Developer" />
-          <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+          <meta
+            name="theme-color"
+            content="#000000"
+          />
+          <meta
+            name="Elijah Ohiwerei"
+            content="Software Developer"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="icons/favicon-16x16.png"
+          />
 
-          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="manifest"
+            href="/manifest.json"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&family=Syncopate:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
 
           <body>
             <Main />
             <NextScript />
           </body>
         </Head>
-
       </Html>
     );
   }
