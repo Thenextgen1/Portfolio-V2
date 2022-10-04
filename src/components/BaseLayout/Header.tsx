910472ca041806e570ff8fdc6630f1ee3ca218f2
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-04 13:49:31 +0100

    Created mobile menu

--- src/components/BaseLayout/Header.tsx
+++ src/components/BaseLayout/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { DesktopNav } from "../../components";
+import { DesktopNav, MobileNav } from "../../components";
 
 const Header = () => {
   return (
-    <header className="py-16">
+    <header className="lg:py-16 py-8 px-8 lg:pl-0">
       <DesktopNav />
+      <MobileNav />
     </header>
   );
 };
--- src/components/BaseLayout/Menus/MobileNav.tsx
+++ src/components/BaseLayout/Menus/MobileNav.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Link from "next/link";
+import styles from "../../../styles/MobileNav.module.css";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <nav className="flex justify-between items-center lg:hidden">
+      <h1 className=" font-semibold tracking-wider">
+        <Link href="/">ELIJAH OHIWEREI</Link>
+      </h1>
+      <div className={styles.line_container}>
+        <div className="mb-1"></div>
+        <div className="mt-1"></div>
+      </div>
+    </nav>
+  );
 };
 
 export default MobileNav;
