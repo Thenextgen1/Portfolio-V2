5636d23fa9ac4df0997776c72f4a84a9886758eb
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-04 04:05:23 +0100

    updated exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Header } from "./BaseLayout/Header";
 export { default as Footer } from "./BaseLayout/Footer";
 export { default as DesktopNav } from "./BaseLayout/Menus/DesktopNav";
 export { default as MobileNav } from "./BaseLayout/Menus/MobileNav";
+export { default as Aside } from "./BaseLayout/Aside";
