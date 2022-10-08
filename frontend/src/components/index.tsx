4521fc19ef13d6892cac4ebd82705f5cb593cbd3
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-08 20:40:09 +0100

    refactored base code

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,4 +3,4 @@ export { default as Footer } from "./BaseLayout/Footer";
 export { default as DesktopNav } from "./BaseLayout/Menus/DesktopNav";
 export { default as MobileNav } from "./BaseLayout/Menus/MobileNav";
 export { default as Aside } from "./BaseLayout/Aside";
-export { default as Experience } from "./Experience";
+export { default as Experiences } from "./Experiences";
