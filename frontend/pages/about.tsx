26fd0277c4085d2e18a107fb044d98618b6e4d27
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-10 23:22:47 +0100

    completed about page

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,20 +1,25 @@
 import BaseLayout from "../src/layout";
 import styles from "../src/styles/About.module.css";
-import { Experience } from "../typings";
+import { Experience, TechStack } from "../typings";
 import { groq } from "next-sanity";
 import { GetStaticProps } from "next";
 import { sanityClient } from "../sanity";
-import { Experiences } from "../src/components";
+import { Experiences, Stack } from "../src/components";
 
 type Props = {
   experience: Experience[];
+  techStack: TechStack[];
 };
 
 const query = groq`
 *[_type == "experience"]
 `;
 
-const About = ({ experience }: Props) => {
+const stackQuery = groq`
+*[_type == "techStack"]
+`;
+
+const About = ({ experience, techStack }: Props) => {
   return (
     <BaseLayout>
       <main className="lg:ml-[16.5%] lg:mr-[20%] px-8 lg:px-0 snap-y snap-mandatory">
@@ -48,11 +53,65 @@ const About = ({ experience }: Props) => {
           </div>
         </section>
 
-        <section className="lg:mt-32 mt-16">
-          <h2 className="font-Synocopate lg:text-[23px] font-semibold opacity-90">
+        <section className="lg:mt-36 mt-16">
+          <h2 className="font-Synocopate lg:text-[23px] font-semibold opacity-90 mb-12">
             EXPERIENCE
           </h2>
-          <Experiences experience={experience} />
+          <div className="mt-6 pb-10">
+            {experience?.map((experience) => (
+              <Experiences
+                key={experience._id}
+                experience={experience}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section className="mt-28">
+          <h2 className="font-Synocopate lg:text-[23px] font-semibold opacity-90 mb-8">
+            TECH STACK
+          </h2>
+          <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between">
+            {techStack?.map((techStack) => (
+              <Stack
+                key={techStack._id}
+                techStack={techStack}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section className="lg:mt-56 mt-12">
+          <h4 className="text-xl lg:text-5xl tracking-wide font-Synocopate font-semibold">
+            SAY <br /> <span className={`${styles.text_stroke}`}> HELLO</span>
+          </h4>
+
+          <p className={styles.mail_link}>
+            <a
+              href="mailto:elijahohimairei@gmail.com"
+              rel="noreferrer"
+              target="_blank"
+            >
+              hello@elijahO.com
+            </a>
+          </p>
+        </section>
+
+        <section className="uppercase lg:pb-8 lg:mt-40 mt-12 opacity-75 text-sm tracking-widest flex flex-col-reverse sm:flex-row sm:justify-between sm:items-center">
+          <div className="py-16">
+            <h5>&#169; 2022 - Elijah Ohiwerei</h5>
+          </div>
+          <ul className={styles.about_links}>
+            <li>
+              <a href="">Linkedin</a>
+            </li>
+            <li className="sm:px-10 py-6 sm:py-0">
+              <a href="">Github</a>
+            </li>
+            <li>
+              <a href="">Dribble</a>
+            </li>
+          </ul>
         </section>
       </main>
     </BaseLayout>
@@ -63,11 +122,14 @@ export default About;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const res = await sanityClient.fetch(query);
+  const stackRes = await sanityClient.fetch(stackQuery);
   const experience: Experience[] = res;
+  const techStack: TechStack[] = stackRes;
 
   return {
     props: {
       experience,
+      techStack,
     },
     // Re-generate the page at most once every 10 second
 
