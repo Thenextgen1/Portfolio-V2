101ebb26ad04f9732da9f49e3912a02d562dff4d
Thenextgen1 <elijahohimairei@gmail.com>
2022-10-13 03:53:44 +0100

    added wrapper for all exit animations on route change

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,16 @@
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import "../src/styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return (
-			<Component {...pageProps} />
-
-	);
+  const router = useRouter();
+  return (
+    <AnimatePresence mode="wait">
+      <Component
+        {...pageProps}
+        key={router.asPath}
+      />
+    </AnimatePresence>
+  );
 }
